b2421425661677eb052beaeaec2d01b36dd266ba
Pankajkumar2608 <tarachandji104@gmail.com>
2026-05-01 08:52:40 +0530

    changed the rentainly api key

--- my-app/src/app/providers.tsx
+++ my-app/src/app/providers.tsx
@@ -4,7 +4,7 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined') {
-  posthog.init('pk_live_a1e62505a24d4ee594193605', {
+  posthog.init('pk_live_7648e9feb64649559c80159f', {
     api_host: 'https://retainly-ingest-worker.kashyap11ayush02.workers.dev/',
     person_profiles: 'identified_only',
   })
